0f04c6fc7ae6f47a86db4c2e8ce564d26a45c5ce
Shakil Khan <dev.shakilkhan@gmail.com>
2024-10-26 22:53:02 +0600

    styled

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,19 +1,20 @@
 export default function Friends({ friend }) {
-    // console.log(friend);
-    const styleFriend = {
-      border: "1px solid red",
-      padding: "10px",
-      margin: "5px",
-        borderRadius: "5px",
-      gap: "10px"
-    };
-    const { name, Age, Address } = friend
-    console.log(name)
-    return (
-      <div style={styleFriend}>
-        <p>Name: {name}</p>
-            <p>Age: {Age}</p>
-            <p>Address : {Address} </p>
-      </div>
-    );
+  // console.log(friend);
+  const styleFriend = {
+    border: "4px solid red",
+    padding: "10px",
+    margin: "5px",
+    borderRadius: "10px",
+      backgroundColor: "#ffff",
+      color: "black",
+  };
+  const { name, Age, Address } = friend;
+  console.log(name);
+  return (
+    <div style={styleFriend}>
+      <p>Name: {name}</p>
+      <p>Age: {Age}</p>
+      <p>Address : {Address} </p>
+    </div>
+  );
 }
